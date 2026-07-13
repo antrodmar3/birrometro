e639f294f6d2e8ef502da4b9bf119c8e22ae1206
antrodmar3 <71787897+antrodmar3@users.noreply.github.com>
2026-07-13 05:52:30 +0200

    Sustituye las iniciales por iconos de cerveza

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "cervezometro-v13";
+const CACHE = "cervezometro-v14";
 const ASSETS = ["./", "index.html", "styles.css", "script.js", "manifest.webmanifest", "icon.svg"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
